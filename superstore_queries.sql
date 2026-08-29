40c69f799cf6391d68a0d658a87a9158bcbcd7c6
Sankalp-Yanamandra <174676664+Sankalp-Yanamandra@users.noreply.github.com>
2026-08-29 19:35:06 +0530

    day 4: upload dataset from kaggle, upload dataset sql script, DISTINCT, ORDER BY

---
+++ superstore_queries.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE superstore;
+
+USE superstore;
+
+SHOW DATABASES;
+SHOW TABLES;
+
+SELECT *
+FROM superstore;
+
+# I. DISTINCT 
+
+# just want specific 3 columns' data
+SELECT Category, Country, segment
+FROM superstore; # has lot of duplicates
+
+# only country column
+SELECT country
+FROM superstore; # lot of duplicates
+
+# distinct
+SELECT DISTINCT country
+FROM superstore; # only unique rows/tuples from this column
+
+# distint on multiple columns
+SELECT DISTINCT category,country,segment
+FROM superstore; # only a unique set of values/rows/records.tuples
+#(1 value of each record comes from each of the 3 columns) taken as a WHOLE STRIP is UNIQUE.
+
+# II. ORDER BY
+
+# unsorted
+SELECT DISTINCT country
+FROM superstore;
+
+# sort ascending 
+SELECT DISTINCT country
+FROM superstore
+ORDER BY country ASC;
+
+# sort descending
+SELECT DISTINCT country
+FROM superstore
+ORDER BY country DESC;
+
+# eg. distinct unsorted
+SELECT DISTINCT country, sales
+FROM superstore;
+
+# sorted asc by country
+SELECT DISTINCT country, sales
+FROM superstore
+ORDER BY country ASC;
+
+# sorted desc by country
+SELECT DISTINCT country, sales
+FROM superstore
+ORDER BY country DESC;
+
+
